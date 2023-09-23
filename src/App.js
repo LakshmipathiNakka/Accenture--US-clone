26bfaef70704d0364b7eb3ad0f1855e90f557256
Lakshmeepathi <lakshmeepathin184@gmail.com>
2023-09-23 11:05:50 +0000

    added responsive ResearchCards Component and imported it to App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import ResearchCards from './components/ResearchCards'
 
 import './App.css'
 
 const App = () => (
   <div className="app-container">
     <Header />
+    <ResearchCards />
   </div>
 )
 
